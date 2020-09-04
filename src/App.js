e3b3d6d1b2a907ca0c7f1b0250ae7d6e25c6cda9
Mael <mael.thomas@data.gouv.fr>
2020-09-04 15:11:17 +0200

    Template

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mon compte mobilité</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Mon compte mobilité</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
